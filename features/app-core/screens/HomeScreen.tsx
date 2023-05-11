422fa99956eae101a33ae537cc1a19d8900c4ada
codinsonn <thorr@codinsonn.dev>
2023-05-11 11:18:07 +0200

    chore: Remove boilerplate, reset styling

--- features/app-core/screens/HomeScreen.tsx
+++ features/app-core/screens/HomeScreen.tsx
@@ -1,20 +1,13 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 // Navigation
-import { Link, fetchAetherProps, useAetherNav, useAetherRoute } from 'aetherspace/navigation'
+import { Link, fetchAetherProps, useAetherRoute } from 'aetherspace/navigation'
 // Schemas
 import { z, aetherSchema, AetherProps } from 'aetherspace/schemas'
 // Primitives
-import { View, Text, Image, Pressable } from 'aetherspace/primitives'
+import { View, Text, Pressable } from 'aetherspace/primitives'
 // SEO
 import { H1 } from 'aetherspace/html-elements'
-// Hooks
-import { useDocAddress, useAPICheck } from 'aetherspace/docs'
-import { useAetherContext } from 'aetherspace/context'
-// Utils
-import { getEnvList } from 'aetherspace/utils'
-// Icons
-import { GraphIcon, ReactIcon, ExpoIcon, StorybookIcon, NextIcon } from '../icons'
 
 /* --- Schemas & Types ------------------------------------------------------------------------- */
 
@@ -70,84 +63,35 @@ export const screenConfig = {
 export const HomeScreen = (props: AetherProps<typeof HomePropsSchema>) => {
   // Props & Data
   const [pageData] = useAetherRoute(props, screenConfig)
-  const { customGreeting, alive, kicking } = pageData
-
-  // Environment
-  const appURIs = getEnvList('APP_LINKS').filter((url) => url.includes('http')) || [] // prettier-ignore
-
-  // Hooks
-  const { isPhoneSize } = useAetherContext()
-  const docsURI = useDocAddress()
-  const { healthEndpoint, graphQLEndpoint } = useAPICheck(appURIs)
-  const { openLink } = useAetherNav()
-
-  // Vars
-  const tapOrClick = isPhoneSize ? 'Tap' : 'Click'
-  const ICON_COLOR = '#22c55e'
-  const ICON_SIZE = 32
 
   // -- Render --
 
   return (
-    <View class="relative flex w-full h-full items-center justify-center">
+    <View class="relative flex w-full h-full items-center justify-center bg-black">
       <StatusBar style="auto" />
-      <Link to="/author">
-        <Image
-          src="/img/icon.png"
-          alt="App Icon"
-          class={[
-            'w-20 h-20 mt-0 mb-3 overflow-hidden bg-slate-100',
-            true && 'rounded-full', // Assign conditional classes with an array
-          ]}
-        />
-      </Link>
-      <H1 class="text-green-500 pb-2 roboto-bold font-bold text-base">{customGreeting}</H1>
-      <View class="flex-row">
-        <Link href="https://expo.dev/home" class="px-2">
-          <ExpoIcon width={ICON_SIZE} height={ICON_SIZE} fill={ICON_COLOR} />
-        </Link>
-        <Link href="https://nextjs.org/" class="px-2">
-          <NextIcon width={ICON_SIZE} height={ICON_SIZE} fill={ICON_COLOR} />
-        </Link>
-        <Link href="https://reactnative.dev/" class="px-2">
-          <ReactIcon width={ICON_SIZE} height={ICON_SIZE} fill={ICON_COLOR} />
-        </Link>
-        <Link href="https://storybook.js.org/docs/react/why-storybook" class="px-2">
-          <StorybookIcon width={ICON_SIZE} height={ICON_SIZE} fill={ICON_COLOR} />
-        </Link>
-        <Link href="https://www.apollographql.com/docs/intro/benefits" class="px-2">
-          <GraphIcon width={ICON_SIZE} height={ICON_SIZE} fill={ICON_COLOR} />
-        </Link>
-      </View>
-      <Text class="md:w-2/3 lg:w-1/2 pt-5 pb-3 px-4 text-center text-sm">
-        {`${tapOrClick} the icons to learn more about each part of the stack, or open up `}
-        <Text class="text-gray-500">features/app-core/screens/HomeScreen.tsx</Text> to start working
-        on your app.
-      </Text>
+      <H1 class="text-white pb-2 roboto-bold font-bold text-lg">Bluetooth Hackathon</H1>
+      <Text class="md:w-2/3 lg:w-1/2 pt-5 pb-3 px-4 text-center text-sm text-white">...</Text>
       <View class="flex-row pt-3">
         <Pressable
-          class="flex-row py-1 px-2 mx-1 bg-gray-700 items-center rounded-md"
-          onPress={() => openLink(graphQLEndpoint || '/api/graphql')}
+          class="flex-row py-1 px-2 mx-1 bg-white items-center rounded-md"
+          // onPress={() => openLink(graphQLEndpoint || '/api/graphql')}
           accessibilityRole="button"
         >
-          <Text class="text-white roboto-bold text-sm">{alive ? 'GraphQL ✅' : 'GraphQL 🔄'}</Text>
+          <Text class="text-black roboto-bold text-sm">SomeButton</Text>
         </Pressable>
         <Pressable
-          class="flex-row py-1 px-2 mx-1 bg-gray-700 items-center rounded-md"
-          onPress={() => openLink(healthEndpoint || '/api/health')}
+          class="flex-row py-1 px-2 mx-1 bg-white items-center rounded-md"
+          // onPress={() => openLink(healthEndpoint || '/api/health')}
           accessibilityRole="button"
         >
-          <Text class="text-white roboto-bold text-sm">{kicking ? 'REST ✅' : 'REST 🔄'}</Text>
+          <Text class="text-black roboto-bold text-sm">OtherButton</Text>
         </Pressable>
       </View>
-      <Link href="/author" class="roboto-bold pt-5 text-center text-sm text-black">
-        Test Navigation
-      </Link>
-      <Link to={`${docsURI}?path=/story/readme-md--page`} class="text-xs roboto-bold my-4 px-5">
-        Read the Docs
-      </Link>
-      <Link to="/author" class="m-2 text-xs text-gray-500">
-        {'{ ...💚 }'}
+      <Link
+        href="https://github.com/thgh/heart-coherence-app#getting-started-%EF%B8%8F"
+        class="roboto-bold pt-5 text-center text-sm text-black"
+      >
+        Github Repo
       </Link>
     </View>
   )
